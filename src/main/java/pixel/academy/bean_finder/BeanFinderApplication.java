ef0f07ba0d27c87d30890a5d37a066a116996920
Sorin Gore <soringoredev@gmail.com>
2025-02-21 22:08:05 +0200

    Initial commit.

---
+++ src/main/java/pixel/academy/bean_finder/BeanFinderApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.bean_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanFinderApplication.class, args);
+	}
+
+}
